1edc1317f6e76fb19647bb2fb9d4009766345f60
4Vergara <cavergara80@uan.edu.co>
2024-05-09 15:47:11 -0500

    Se agregan los archivos que no se cargaron

---
+++ app/src/main/java/com/example/proyecto_electiva/AdaptadorEjecicio.kt
@@ -0,0 +1,32 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto_electiva.R
+import com.example.proyecto_electiva.elementoEjercicio
+
+class AdaptadorEjercicio(private val datos: ArrayList<elementoEjercicio>, private val context: Context) : RecyclerView.Adapter<AdaptadorEjercicio.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titulo: TextView = itemView.findViewById(R.id.titulo_ejercicio)
+        val imagen: ImageView = itemView.findViewById(R.id.imagen_ejercicio)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.lista_ejercicio, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = datos[position]
+        holder.titulo.text = item.nombre
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
+    }
+}
+
